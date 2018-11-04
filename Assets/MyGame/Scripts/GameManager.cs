7240bc087cee1d4304a0a16bcb5ca93c29bdf285
Sarah Loos <s.loos@hotmail.com>
2018-11-04 22:17:08 +0100

    initial commit

---
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnGameStarted;
+    public static event PlayerDelegate OnGameOverConfirmed;
+
+    public static GameManager Instance;
+
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public GameObject countdownPage;
+    public Text scoreText;
+
+    enum PageState
+    {
+        None,
+        Start,
+        GameOver,
+        Countdown
+
+    }
+
+    int score = 0;
+    bool gameOver = true;
+
+    public bool GameOver { get { return gameOver; } }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void OnEnable()
+    {
+        CountdownText.OnCountdownFinished += OnCountdownFinished;
+        TapController.OnPlayerDied += OnPlayerDied;
+        TapController.OnPlayerDied += OnPlayerScored;
+
+    }
+
+    void OnDisable()
+    {
+        CountdownText.OnCountdownFinished -= OnCountdownFinished;
+        TapController.OnPlayerDied -= OnPlayerDied;
+        TapController.OnPlayerScored -= OnPlayerScored;
+    }
+
+    void OnCountdownFinished() {
+
+        SetPageState(PageState.None);
+        OnGameStarted();
+        score = 0;
+        gameOver = false;
+    }
+
+    void OnPlayerDied() {
+        gameOver = true;
+        int savedScore = PlayerPrefs.GetInt("Highscore");
+        if (score > savedScore) {
+            PlayerPrefs.SetInt("Highscore", score);
+
+        }
+        SetPageState(PageState.GameOver);
+    }
+
+    void OnPlayerScored() {
+
+        score++;
+        scoreText.text = score.ToString();
+
+    }
+
+    void SetPageState(PageState state)
+    {
+        switch (state)
+        {
+
+            case PageState.None:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+
+                break;
+
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(false);
+
+                break;
+
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(true);
+                countdownPage.SetActive(false);
+
+                break;
+
+            case PageState.Countdown:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownPage.SetActive(true);
+
+                break;
+
+
+        }
+
+    }
+
+    public void ConfirmGameOver()
+    {
+
+        OnGameOverConfirmed();
+        scoreText.text = "0";
+        SetPageState(PageState.Start);
+    }
+
+    public void StartGame()
+    {
+        SetPageState(PageState.Countdown);
+
+    }
+}
--- README.md
+++ README.md
@@ -8,4 +8,8 @@ Desc: A player has to lead the bird through the rails without touching them. To 
 ### Game Tutorial:
 ![tutorial](./Screenshots/tutorial.png)
 [link for tutorial](https://www.youtube.com/watch?v=A-GkNM8M5p8)
+
+# sources
+[link for donald trump pic](https://www.clipartmax.com/middle/m2i8d3K9Z5Z5A0i8_free-clipart-of-donald-trump-waving-impeachment-all-the-reasons-donald-trump/)
+
 ----
